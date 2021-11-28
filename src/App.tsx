c65607323003ed3b668566bfa06b32596d74959b
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-11-29 01:29:33 +0330

    create default page layout and stylings

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import styled from "@emotion/styled"
+
+const MainPage = styled.main`
+	color: #555;
+	background-color: #eee;
+	height: 100vh;
+`
+const Header = styled.header`
+	position: fixed;
+	height: 6rem;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	background: #ffffff;
+	padding: 0 4rem;
+`
+const Title = styled.p`
+	font-size: 2rem;
+`
+const Container = styled.div`
+	height: 100%;
+	width: 100%;
+	padding: 6rem 1rem 0 1rem;
+	display: flex;
+	justify-content: center;
+	flex-wrap: wrap;
+`
+const Card = styled.div`
+	background-color: white;
+	border-radius: 4px;
+	box-shadow: 2px 2px 4px #aaa;
+	margin: 1rem;
+	flex: 1 1 15rem;
+	&:nth-child(2) {
+		flex: 3 1 30rem;
+	}
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<MainPage>
+			<Header>
+				<Title>Crypto</Title>
+			</Header>
+			<Container>
+				<Card>ddd</Card>
+				<Card>ddd</Card>
+			</Container>
+		</MainPage>
+	)
 }
 
-export default App;
+export default App
