0d855213e37d32acd192589d9da6ec7f98d326d8
Mahdi Majdian <mahdi2011.shz@gmail.com>
2021-11-30 00:12:14 +0330

    add Coin type

---
+++ src/types.ts
@@ -0,0 +1,18 @@
+export interface ICoin {
+	baseCurrency: string
+	baseIncrement: string
+	baseMaxSize: string
+	baseMinSize: string
+	enableTrading: boolean
+	feeCurrency: string
+	isMarginEnabled: boolean
+	market: string
+	name: string
+	priceIncrement: string
+	priceLimitRate: string
+	quoteCurrency: string
+	quoteIncrement: string
+	quoteMaxSize: string
+	quoteMinSize: string
+	symbol: string
+}
